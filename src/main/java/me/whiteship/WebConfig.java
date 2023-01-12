d07988af3708cd34a2d6567a5c67d7e77f4d49ef
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-12 21:57:45 +0900

    스프링 MVC 연동

--- src/main/java/me/whiteship/WebConfig.java
+++ src/main/java/me/whiteship/WebConfig.java
@@ -2,9 +2,8 @@ package me.whiteship;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
 
 @Configuration
-@ComponentScan(useDefaultFilters = false, includeFilters = @ComponentScan.Filter(Controller.class))
+@ComponentScan
 public class WebConfig {
 }
