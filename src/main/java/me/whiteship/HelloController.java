9105c45222664c5d967f8522209d970c4db9d0f1
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-13 22:58:37 +0900

    스프링 MVC 빈 설정

--- src/main/java/me/whiteship/HelloController.java
+++ src/main/java/me/whiteship/HelloController.java
@@ -3,6 +3,8 @@ package me.whiteship;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,9 +13,15 @@ public class HelloController {
     @Autowired
     HelloService helloService;
 
-    @GetMapping("/hello")
+    /**
+     * /hello/1?name=keesun&age=25
+     * @param id
+     * @param name
+     * @return
+     */
+    @GetMapping("/hello/{id}")
     @ResponseBody
-    public String hello() {
+    public String hello(@PathVariable int id, @RequestParam String name) {
         return "Hello, " + helloService.getName();
     }
 
