9109aae01cf8a9513300b75869f791cb62a0caa6
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-13 23:32:36 +0900

    WebMvcConfigurer

--- src/main/java/me/whiteship/WebConfig.java
+++ src/main/java/me/whiteship/WebConfig.java
@@ -1,21 +1,17 @@
 package me.whiteship;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan
 @EnableWebMvc
-public class WebConfig {
-    @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
+public class WebConfig implements WebMvcConfigurer {
+
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF.", ".jsp");
     }
 }
